6cf10d4d0986da2916a4b7cfc43267591040d999
Sharma <Ashwani.Sharma@McKesson.com>
2020-09-16 16:15:59 +0530

    updated project , dataset and scheduler name

---
+++ files/main.py
@@ -0,0 +1,168 @@
+"""This script imports a google sheet and inputs
+it into BigQuery"""
+from __future__ import print_function
+import base64
+import sys
+import os.path
+#import csv
+import pandas as pd
+import logging
+import json
+from datetime import date
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+# If modifying these scopes, delete the file token.pickle.
+scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+class SheetService:
+    """
+      Main Sheet class to handle all sheet operations.
+    """
+    def __init__(self, sheet_version='v4', credentials=None):
+        """ Initialize the class for the api versions etc..
+
+        Args:
+            compute_version_id: The compute api version to use. Defaults to v1
+            resource_version_id: The cloud resource manager version to use. Defaults to v1
+            max_results: The max number of results to fetch in each call to the API. Defaults to 500
+        """
+        if credentials:
+            self.sheet_client = build('sheets', sheet_version,
+              credentials=credentials, cache_discovery=False)
+            self.bq_client = bigquery.Client()
+        else:
+            self.sheet_client = build('sheets',
+              sheet_version, cache_discovery=False)
+            self.bq_client = bigquery.Client()
+        self.sheets = self.sheet_client.spreadsheets()
+        self.logger = logging.getLogger(__name__)
+        self.errors = []
+        self.columns = []
+        self.values = []
+
+
+    def _set_values(self, sheet_id, sheet_range):
+        """ Calls the Sheet API to get the actual results of the sheets
+
+        Args:
+            sheet_id: The google sheet id we want to grab
+            sheet_range: the range of which we require.
+        """
+        try:
+            result = self.sheets.values().get(spreadsheetId=sheet_id,
+                                    range=sheet_range).execute()
+            self.columns = result.get('values',[]).pop(0)
+            for i in range(0,len(self.columns)):
+                self.columns[i] = self.columns[i].replace(' ','_')
+
+            self.values = result.get('values', [])
+        except Exception as ex:
+            message = "An exception occured attempting to \
+              open sheet {} on range {} with message {}".format(sheet_id,sheet_range, ex)
+            self.logger.error(message)
+            sys.exit()
+
+    def load_into_bq(self,table_id,date_append=True):
+        """ Uploads the presumably CSV file to the given gcs bucket
+        appends current date for a new table to keep historical information
+        """
+        if date_append:
+            table_id = "{}_{}".format(table_id, date.today().strftime("%Y%m%d"))
+        
+        dataFrame = pd.DataFrame(data=self.values,columns=self.columns)
+
+        print("Starting on table: {}".format(table_id))
+        job_config = bigquery.LoadJobConfig(
+            autodetect=True,
+            write_disposition="WRITE_TRUNCATE")
+        job = self.bq_client.load_table_from_dataframe(dataFrame,
+            table_id, job_config=job_config)
+
+        job.result()
+        destination_table = self.bq_client.get_table(table_id)  # Make an API request.
+        print("Loaded {} rows.".format(destination_table.num_rows))
+
+    def workflow(self, sheet_id, sheet_range, csv_name, table_id):
+        """ Runs the full workflow of gathering the sheet and data
+
+        Args:
+            sheet_id: The google sheet id we want to grab
+            sheet_range: the range of which we require.
+        """
+        logging.info("workflow starting for sheet_id: %s, range: %s, \
+                      csv_name: %s, table_id: %s", sheet_id,
+                      sheet_range, csv_name, table_id)
+        self._set_values(sheet_id=sheet_id, sheet_range=sheet_range)
+        self.load_into_bq(table_id=table_id)
+
+def wrapper(sample_data,credentials=None):
+    """To make local testing mirror a Cloud function as much as possible
+    have both call the same wrapper.
+    """
+    for cur_sheet in sample_data["sheets"]:
+        logging.info("Initializing sheet service for cur_sheet: {}".format(cur_sheet))
+        sheet_service = SheetService(sheet_version='v4', credentials=credentials)
+        logging.info("SheetService initialized, begining workflow".format(cur_sheet))
+        sheet_service.workflow(sample_data["sheet_id"],
+          sheet_range=cur_sheet["range"],
+          csv_name=cur_sheet["name"]+".csv",
+          table_id="{}.{}".format(sample_data["data_set"],cur_sheet["name"]))
+
+        if not sheet_service.values:
+            print('No data found.')
+        else:
+            print("sucessfully completed current sheet: {}".format(cur_sheet))
+
+def sheet_pubsub(event, context):
+    """Background Cloud Function to be triggered by Pub/Sub.
+    Args:
+          event (dict):  The dictionary with data specific to this type of
+          event. The `data` field contains the PubsubMessage message. The
+          `attributes` field will contain custom attributes if there are any.
+          context (google.cloud.functions.Context): The Cloud Functions event
+          metadata. The `event_id` field contains the Pub/Sub message ID. The
+          `timestamp` field contains the publish time.
+    """
+
+    print("""This Function was triggered by messageId {} published at {}
+    """.format(context.event_id, context.timestamp))
+
+    if 'data' in event:
+        # Grab and decode the payload
+        payload = base64.b64decode(event['data']).decode('utf-8')
+        sheet_info = json.loads(payload)
+        print("Sheet info: {}".format(sheet_info))
+        wrapper(sample_data=sheet_info)
+    else:
+        print("No data found in event. Exiting.")
+
+def main():
+    """If Main is executed you are doing this locally and will need the json file
+      if being executed via the cloud function we should have the permissions within the
+      service account executing the function.
+    """
+    # The ID and range of a sample spreadsheet.
+    sample_data = {
+        "sheet_id" : "1oRULNbJj5sG7HsVvA4ySD5w3LOX_P_-g5nvazkk2z58",
+        "data_set" : "mig-dashboard-dev-e918.mck_dashboard_data",
+        "sheets" : [
+                {"name": "dc_cogent", "range": "Cogent Dashboard!F1:J25"},
+                {"name": "dc_markham", "range": "Markham Dashboard!F1:J25"},
+        ]
+    }
+    logging.info("Executing via main, looking for a credentials file")
+    if os.path.exists('credentials.json'):
+        secret_file = os.path.join(os.getcwd(), 'credentials.json')
+        credentials = service_account.Credentials.from_service_account_file(
+          secret_file, scopes=scopes)
+        wrapper(sample_data=sample_data, credentials=credentials)
+
+    else:
+        logging.error("No credential file found, running in Google?")
+        wrapper(sample_data=sample_data)
+
+if __name__ == '__main__':
+    main()
+    sys.exit()
--- variables.tf
+++ variables.tf
@@ -31,10 +31,6 @@ variable "cf_service_account_email" {
   default = "mig-dashboard-dev-e918-1@mig-dashboard-dev-e918.iam.gserviceaccount.com"
 }
 
-variable "cf_subnet_ip" {
-  type    = string
-  default = "10.132.0.0/28"
-}
 
 variable "sheet_information" {
   type = object({
@@ -43,8 +39,8 @@ variable "sheet_information" {
     sheets   = list(map(string))
   })
   default = {
-    sheet_id       = "my_private_sheet123456789"
-    data_set = "myproject.data_set"
+    sheet_id = "1oRULNbJj5sG7HsVvA4ySD5w3LOX_P_-g5nvazkk2z58"
+    data_set = "mig-dashboard-dev-e918.mck_dashboard_data"
     sheets = [
       {
         name  = "dc_cogent"
@@ -53,6 +49,22 @@ variable "sheet_information" {
       {
         name  = "dc_markham"
         range = "Markham Dashboard!F1:J25"
+      },
+      {
+        name  = "dc_uniprix"
+        range = "Uniprix Dashboard!F1:J60"
+      },
+      {
+        name  = "dc_viscount"
+        range = "Viscount Dashboard!F1:J15"
+      },
+      {
+        name  = "dc_tracker"
+        range = "DataCenter Tracker!A1:K7"
+      },
+      {
+        name  = "dc_all"
+        range = "Master Server Inventory!A1:BZ1000"
       }
     ]
   }
@@ -67,7 +79,7 @@ variable "app_location" {
 bigquery variables 
 **************************/
 variable "dataset_id" {
-  default     = "mck_dc_workloads"
+  default     = "mck_dashboard_data"
   description = "dashboard dataset id"
 }
 
@@ -85,7 +97,7 @@ variable "mck_views" {
   type        = map(string)
   description = "list of views needs to created"
   default = {
-    mck_dc_dashboard             = <<EOF
+    mck_dc_dashboard               = <<EOF
   SELECT DC, 
   SUM(Planned_InFlight_Migration) AS Planned_InFlight_Migration,
   SUM(Sunset_in_Place) AS Sunset_in_Place , 
@@ -105,24 +117,50 @@ variable "mck_views" {
         COUNTIF(Migrate_to_GCP = 'TRUE') AS Migrate_to_GCP,
         COUNTIF(Unassessed = 'TRUE') AS UnAccessed,
         PARSE_DATE('%Y%m%d',_TABLE_SUFFIX) as Date 
-        FROM `ic-iaprep-mgmt-project-a4d4.mck_dc_workloads.dc_all_*`
+        FROM `mig-dashboard-dev-e918.mck_dashboard_data.dc_all_*`
         GROUP BY Source, _TABLE_SUFFIX 
         ) 
   GROUP BY DC, Date
 EOF
-    mck_dc_all_workloads         = <<EOF
-SELECT  'Cogent' as dc, Unconfirmed_App, Application, Disposition, CAST(Server_Count AS INT64) AS Server_Count , CAST(Desktop_Count AS INT64) AS Desktop_Count ,PARSE_DATE('%Y%m%d',_TABLE_SUFFIX) as Date FROM `ic-iaprep-mgmt-project-a4d4.mck_dc_workloads.dc_cogent_*` union all 
-SELECT  'Markham' as dc,Unconfirmed_App, Application, Disposition, CAST(Server_Count AS INT64) AS Server_Count , CAST(Desktop_Count AS INT64) AS Desktop_Count ,PARSE_DATE('%Y%m%d',_TABLE_SUFFIX) as Date FROM `ic-iaprep-mgmt-project-a4d4.mck_dc_workloads.dc_markham_*` union all 
-SELECT  'Unipri' as dc, Unconfirmed_App, Application, Disposition, CAST(Server_Count AS INT64) AS Server_Count , CAST(Desktop_Count AS INT64) AS Desktop_Count ,PARSE_DATE('%Y%m%d',_TABLE_SUFFIX) as Date FROM `ic-iaprep-mgmt-project-a4d4.mck_dc_workloads.dc_uniprix_*` union all 
-SELECT  'Viscount' as dc, Unconfirmed_App, Application, Disposition, CAST(Server_Count AS INT64) AS Server_Count , CAST(Desktop_Count AS INT64) AS Desktop_Count ,PARSE_DATE('%Y%m%d',_TABLE_SUFFIX) as Date FROM `ic-iaprep-mgmt-project-a4d4.mck_dc_workloads.dc_viscount_*`
+    mck_dc_all_workloads           = <<EOF
+SELECT  'Cogent' as dc, Unconfirmed_App, Application, Disposition, CAST(Server_Count AS INT64) AS Server_Count , CAST(Desktop_Count AS INT64) AS Desktop_Count ,PARSE_DATE('%Y%m%d',_TABLE_SUFFIX) as Date FROM `mig-dashboard-dev-e918.mck_dashboard_data.dc_cogent_*` union all 
+SELECT  'Markham' as dc,Unconfirmed_App, Application, Disposition, CAST(Server_Count AS INT64) AS Server_Count , CAST(Desktop_Count AS INT64) AS Desktop_Count ,PARSE_DATE('%Y%m%d',_TABLE_SUFFIX) as Date FROM `mig-dashboard-dev-e918.mck_dashboard_data.dc_markham_*` union all 
+SELECT  'Unipri' as dc, Unconfirmed_App, Application, Disposition, CAST(Server_Count AS INT64) AS Server_Count , CAST(Desktop_Count AS INT64) AS Desktop_Count ,PARSE_DATE('%Y%m%d',_TABLE_SUFFIX) as Date FROM `mig-dashboard-dev-e918.mck_dashboard_data.dc_uniprix_*` union all 
+SELECT  'Viscount' as dc, Unconfirmed_App, Application, Disposition, CAST(Server_Count AS INT64) AS Server_Count , CAST(Desktop_Count AS INT64) AS Desktop_Count ,PARSE_DATE('%Y%m%d',_TABLE_SUFFIX) as Date FROM `mig-dashboard-dev-e918.mck_dashboard_data.dc_viscount_*`
 EOF
-    mck_dc_all_workloads_details = <<EOF
+    mck_dc_all_workloads_details   = <<EOF
     SELECT Source, Server, OS, Normalized_OS, Power_State, Application_Lookup, Confirmed_Application, MW_Project_Name, Sunset_in_Place,
     Citrix, Modernization, Migrate_to_Azure, Migrate_to_GCP, Unassessed, Migration_Scheduled, Migration_Succeeded, In_flight_Initiative,
-    Project_Owner, Tech_Owner, BAP_ID, Lean_IX_ID, Archer_ID, Host_CPU, Host_Mem,PARSE_DATE('%Y%m%d',_TABLE_SUFFIX) as Date
-    FROM `ic-iaprep-mgmt-project-a4d4.mck_dc_workloads.dc_all_*`
+    Project_Owner, Tech_Owner, BAP_ID, Lean_IX_ID, Archer_ID, Host_CPU, Host_Mem,Allocated_Storage,Rationale, Server_Function_Notes, 
+    Wave, Wave_Order, Environment,Destination_Project,Project_Request_ID, PARSE_DATE('%Y%m%d',_TABLE_SUFFIX) as Date
+  ,(case 
+      when Sunset_in_Place = 'TRUE' then 'Sunset'
+      when Citrix = 'TRUE' then 'Citrix'
+      when Modernization = 'TRUE' then 'Modernization'
+      when Migrate_to_Azure = 'TRUE' then 'Azure'
+      when Migrate_to_GCP = 'TRUE' then 'GCP'
+      when Unassessed = 'TRUE' then 'Unassessed Server ' 
+      else 'other'
+    end) as disposition
+
+    FROM `mig-dashboard-dev-e918.mck_dashboard_data.dc_all_*`
     Order by Date
 EOF
+    mck_dc_all_workloads_dc_rollup = <<EOF
+    SELECT DISTINCT
+    Source
+    ,sum(case when Sunset_in_Place = "TRUE" then 1 else 0 end) AS Sunset_in_Place
+    ,sum(case when Migrate_to_GCP = "TRUE" then 1 else 0 end) AS Migrate_to_GCP
+    ,sum(case when Citrix = "TRUE" then 1 else 0 end) AS Migrate_to_Citrix
+    ,sum(case when Migrate_to_Azure = "TRUE" then 1 else 0 end) AS Migrate_to_Azure
+    ,sum(case when Modernization = "TRUE" then 1 else 0 end) AS Modernization
+    ,sum(case when Unassessed = "TRUE" then 1 else 0 end) AS Unassessed
+    ,date
+    FROM mig-dashboard-dev-e918.mck_dashboard_data.mck_dc_all_workloads_details 
+    WHERE  date = CURRENT_DATE()
+    GROUP BY Source, date
+EOF
+
   }
 }
 
