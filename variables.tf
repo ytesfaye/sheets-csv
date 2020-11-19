dc9e4049568bf2bb1a5235909954687dfc152124
Sharma <Ashwani.Sharma@McKesson.com>
2020-11-19 14:24:53 +0530

    fix: updated missing coloumn in view

--- variables.tf
+++ variables.tf
@@ -124,7 +124,7 @@ variable "smart_sheet_info" {
     sheets = [
       {
         name  = "mck-smart-sheet-dash"
-        range = "Import!A1:N6000"
+        range = "Import!A1:O6000"
       }
     ]
   }
